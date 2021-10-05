314c946b9e8bbd22f39cc84064c8e856f07d15e0
Quyen Trinh <vanquyen1994@gmail.com>
2021-10-05 16:12:15 +0700

    Initial Project.

---
+++ DemoProfject/DemoProject/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DemoProject
+//
+//  Created by Quyen Trinh on 04/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var appNameLabel: UILabel!
+    @IBOutlet weak var appBundleIdLabel: UILabel!
+    @IBOutlet weak var appVersionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        appNameLabel.text = try! Configuration.value(for: "APP_NAME")
+        appBundleIdLabel.text = try! Configuration.value(for: "APP_ID")
+        appVersionLabel.text = try! Configuration.value(for: "APP_VERSION")
+        
+        
+    }
+
+
+}
+
